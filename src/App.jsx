e52ed7c68fc98fa70e2fc4c7637f576bb7dfdeb3
mohamed271220 <mohamedaprahym@gmail.com>
2023-03-03 22:08:25 +0200

    half of acc page done(front end)

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/Account/:subpage?" element={<Account />} />
+          <Route path="/Account/:subpage/:action" element={<Account />} />
  
         </Route>
       </Routes>
